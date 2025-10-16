e71bfb55233f8a2ac467c744c4469f849b690ba8
Debsankha Manik <github@debsankha.net>
2025-10-16 20:28:54 +0200

    this ooms even earlier

--- recipe/retool/run_tiny_poc_arc_rl.sh
+++ recipe/retool/run_tiny_poc_arc_rl.sh
@@ -66,6 +66,8 @@ python3 -m verl.trainer.main_ppo \
     data.custom_cls.name=CustomRLHFDataset \
     custom_reward_function.path=recipe/retool/arcagi2.py \
     custom_reward_function.name=compute_score \
+    actor_rollout_ref.actor.use_torch_compile=False \
+    actor_rollout_ref.ref.use_torch_compile=False \
     actor_rollout_ref.model.path=$model_path \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
